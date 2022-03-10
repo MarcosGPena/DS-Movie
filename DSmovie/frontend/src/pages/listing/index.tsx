ae32c2f62e1ebf5afe51e10b06971d1f97fe239b
Marcos Pena <marrcospena@gmail.com>
2022-03-10 00:39:40 -0300

    MovieCard

--- DSmovie/frontend/src/pages/listing/index.tsx
+++ DSmovie/frontend/src/pages/listing/index.tsx
@@ -1,10 +1,36 @@
 
+import MovieCard from "../../componentes/movieCard";
 import Pagination from "../../componentes/pagination"
+
 
 function Listing(){
     return(
-
+        < >
         <Pagination />
+
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <MovieCard />
+                </div>
+            </div>
+            
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <MovieCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-3">
+                    <MovieCard />
+                </div>
+            </div>
+           
+        </div>
+        
+        </>
+
     );
 }
 
